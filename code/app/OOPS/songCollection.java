f7edbddea8bbb628589d9ccda30d9016d19177be
Saurav Rauniyar <50069184+sarry971@users.noreply.github.com>
2022-01-28 17:55:40 +0545

    Update songCollection.java

--- code/app/OOPS/songCollection.java
+++ code/app/OOPS/songCollection.java
@@ -17,107 +17,71 @@ class Song
     
     // getters
     
-    public int getSongId()
+    public  int getSongId()
     {
         return this.songId;
     }
     
-    public String getTitle()
+    public  String getTitle()
     {
         return this.title;
     }
     
-    public String getArtist()
+    public  String getArtist()
     {
         return this.artist;
     }
     
-    public double getDuration()
+    public  double getDuration()
     {
         return this.duration;
     }
     
     //setters
-    public void setSongId(int songId)
+    public  void setSongId(int songId)
     {
         this.songId = songId;
     }
     
-    public void setTitle(String title)
+    public  void setTitle(String title)
     {
         this.title = title;
     }
     
-    public void setArtist(String artist)
+    public  void setArtist(String artist)
     {
         this.artist = artist;
     }
     
-    public void setDuration(String duration)
+    public  void setDuration(double duration)
     {
         this.duration = duration;
     }
 }
-public class MyClass
+public class Main
 {
-    Map<String, Song[]> songCollection = new HashMap<String, Song>();
+    Map<String, List<Song>> songCollection = new HashMap();
     
-    public MyClass(Map<String, Song[]> collection)
+    Main(Map<String, List<Song>> collection)
     {
         this.songCollection = collection;
     }
     
-    public Map<String, Song[]> getSongCollection()
+    public  Map<String, List<Song>> getSongCollection()
     {
         return this.songCollection;
     }
-    public Song[] getArtist(String songArtist)
+    public  List<Song> getArtist(String songArtist)
     {
         return getSongCollection().get(songArtist);
     }
     
-	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);
-		
-		Map<String, Song[]> songCollection = new HashMap();
-		Song[] song = new Song[5];
-		
-		for(int i=0; i<5; i++)
-		{
-		    int id = sc.nextInt();
-		    String t = sc.next();
-		    String a = sc.next();
-		    double d = sc.nextDouble();
-		    song[i] = new Song(id, t, a, d);
-		    if (!songCollection.containsKey(a))
-		        songCollection.put(a, song[i]);
-		    else 
-		        songCollection.put(a, songCollection.get(a).add(song[i]));
-		}
-		String userArtist = sc.next();
-		double totalTime = findTotalDuration(song, userArtist);
-		if (totalTime != null)
-		    System.out.println("The total duration of "+userArtist+" is of "+ totalTime);
-		else 
-		    System.out.println("The "+userArtist+" is not present");
-		    
-		Song[] sortedDuration = sortedAscending(song, userArtist);
-		
-		if (sortedDuration != null)
-		    {
-		        System.out.println("The songs of "+userArtist+" in ascending order are:");
-		        for(Song s: sortedDuration)
-		            System.out.println(s);
-		    }
-		else 
-		    System.out.println("The "+userArtist+" is not present");
-	}
-	
-	public static double findTotalDuration(Song[] song, String userArtist)
+    	
+	public double findTotalDuration(List <Song> song, String userArtist)
 	{
-	    Song[] s = getArtist(userArtist);
+	    List<Song> s = getArtist(userArtist);
 	    
-	    if (s == null)
+	    if (s.size() == 0)
 	        return 0.0;
 	        
 	    double total = 0.0;
@@ -127,28 +91,63 @@ public class MyClass
 	    return total;
 	}
 	
-	public static Song[] sortedAscending(Song[] song, String userArtist)
+	public  List<Song> sortedAscending(List<Song> song, String userArtist)
 	{
-	    Song[] s = getArtist(userArtist);
+	    List<Song> s = getArtist(userArtist);
 	    
-	    if (s == null)
-	        return null;
+	    if (s.size() == 0)
+	        return s;
 	        
-	    // double total = 0.0;
-	    
-	    for(int i=0; i<s.length(); i++)
-	    {
-	       for(int j = i+1; j<s.length(); j++)
-	       {
-	           if (s[i].getDuration()>s[j].getDuration())
+	    for(int i=0; i<s.size(); i++)
+	       for(int j = i+1; j<s.size(); j++)
+	           if (s.get(i).getDuration()>s.get(j).getDuration())
 	           {
-	               Song temp = s[i];
-	               s[i] = s[j];
-	               s[j] = temp;
+	               int indexI = s.indexOf(i);
+	               int indexJ = s.indexOf(j);
+	               s.set(indexI, s.get(j));
+	               s.set(indexJ, s.get(i));
 	           }
-	       }
-	    }
 	    
 	    return s;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		
+		Map<String, List<Song>> songCollection = new HashMap();
+		List<Song> song = new ArrayList();
+		
+		for(int i=0; i<5; i++)
+		{
+		    int id = sc.nextInt();
+		    String t = sc.next();
+		    String a = sc.next();
+		    double d = sc.nextDouble();
+		    song.add(new Song(id, t, a, d));
+		    
+		    if (!songCollection.containsKey(a))
+		        songCollection.put(a, song);
+		    else 
+		        songCollection.get(a).add(new Song(id, t, a, d));
+		}
+		String userArtist = sc.next();
+		
+		double totalTime = findTotalDuration(song, userArtist);
+		
+	    if (totalTime > 0.0)
+		    System.out.println("The total duration of "+userArtist+" is of "+ totalTime);
+		else 
+		    System.out.println("The "+userArtist+" is not present");
+		    
+	    List<Song> sortedDuration = sortedAscending(song, userArtist);
+		
+		if (sortedDuration.size() > 0)
+		    {
+		        System.out.println("The songs of "+userArtist+" in ascending order are:");
+		        for(Song s: sortedDuration)
+		            System.out.println(s);
+		    }
+		else 
+		    System.out.println("The "+userArtist+" is not present");
 	}
 }
